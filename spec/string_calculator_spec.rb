11aa4b20ab1e582f02a927c83e5f21b34608028b
Nitish Sharma <0007nitishsharma@gmail.com>
2025-09-16 16:42:54 +0530

    addition with default delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,8 +5,17 @@ describe StringCalculator do
   
   describe '#add' do
     it 'returns 0 when the string is empty' do
-      expect(subject.add('')).to eq(0)
+      expect(subject.add("")).to eq(0)
     end
+
+    it 'return the sum of numbers separated by commas' do
+      expect(subject.add("1,2,3")).to eq(6)
+    end
+
+    it 'return the sum of numbers separated by multiple commas' do
+      expect(subject.add("1,2,,3")).to eq(6)
+    end
+
   end
 
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,18 @@
 class StringCalculator
 
+  def initialize
+    @delimiters = [',']
+  end
+
   def add(input)
     return 0 if input.empty?
+
+    @delimiters.each do |delimiter|
+      input = input.gsub(delimiter, ',')
+    end
+
+    output = input.split(',').map(&:to_i).sum
+    output
   end
 
 end
